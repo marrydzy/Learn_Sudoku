619a3e81924dcb6e7894956037d87a9ff6569d6a
Marek Rydzy <marek.rydzy@gmail.com>
2021-07-26 19:53:20 +0200

    Test 3 refactored - phase I

--- uniqueness_tests.py
+++ uniqueness_tests.py
@@ -157,7 +157,7 @@ def test_3(solver_status, board, window):
     By treating these two cells as one node, find k - 1 other cells (as nodes)
     in the same house as these two cells so that the union of the candidates
     for these k cells has exactly k unique digits. Then the Naked Subset rule
-    can be applied eliminate these k digits from the other cells in the house.
+    can be applied to eliminate these k digits from the other cells in the house.
     The algorithm is implemented for k = 2, 3 and 4
     Rating: 100
     """
@@ -167,8 +167,8 @@ def test_3(solver_status, board, window):
         color_1 = 'yellow'
         color_2 = 'lime'
 
-        c_chain[ceiling_a] = {(z_values[0], 'cyan')}
-        c_chain[ceiling_b] = {(z_values[1], 'cyan')}
+        c_chain[ceiling_a] = {(z, 'cyan') for z in z_values}
+        c_chain[ceiling_b] = {(z, 'cyan') for z in z_values}
         for cell in naked_subset:
             for value in board[cell]:
                 c_chain[cell].add((value, 'cyan'))
@@ -181,15 +181,26 @@ def test_3(solver_status, board, window):
             color_1 = 'lime' if color_1 == 'yellow' else 'yellow'
             color_2 = 'lime' if color_2 == 'yellow' else 'yellow'
 
-    def _find_xyz(x, y, cells, by_row):
-        if by_row:
-            return {CELL_ROW[cell] for cell in cells if len(board[cell]) == 3 and x in board[cell] and y in board[cell]}
-        else:
-            return {CELL_COL[cell] for cell in cells if len(board[cell]) == 3 and x in board[cell] and y in board[cell]}
+    def _find_xyz(n, x, y, cells_a, cells_b):
+        x_ids = set()
+        for id_x in range(9):
+            if x in board[cells_a[id_x]] and y in board[cells_a[id_x]] and \
+                    x in board[cells_b[id_x]] and y in board[cells_b[id_x]] and \
+                    2 < len(board[cells_a[id_x]]) <= n + 2 and \
+                    2 < len(board[cells_b[id_x]]) <= n + 2:
+                candidates = set(board[cells_a[id_x]]).union(board[cells_b[id_x]])
+                if len(candidates) <= n + 2:
+                    x_ids.add(id_x)
+        return x_ids
 
-    def _naked_subset(n, ceiling_a, ceiling_b, bi_value, z_a, z_b, by_row):
+        # if by_row:
+        #     return {CELL_ROW[cell] for cell in cells if x in board[cell] and y in board[cell]}
+        # else:
+        #     return {CELL_COL[cell] for cell in cells if x in board[cell] and y in board[cell]}
+
+    def _naked_subset(n, ceiling_a, ceiling_b, bi_value, z, by_row):
         cells = {cell for cell in set(ALL_NBRS[ceiling_a]).intersection(ALL_NBRS[ceiling_b]) if len(board[cell]) > 1}
-        nodes = {cell for cell in cells if len(board[cell]) <= n and (z_a in board[cell] or z_b in board[cell])
+        nodes = {cell for cell in cells if len(board[cell]) <= n and set(board[cell]).intersection(z)
                  and not set(board[cell]).intersection(bi_value)}
         houses = [nodes.intersection(
             CELLS_IN_ROW[CELL_ROW[ceiling_a]] if by_row else CELLS_IN_COL[CELL_COL[ceiling_a]])]
@@ -197,7 +208,7 @@ def test_3(solver_status, board, window):
             houses.append(nodes.intersection(CELLS_IN_SQR[CELL_SQR[ceiling_a]]))
         for house in houses:
             for subset in combinations(house, n-1):
-                values = set("".join(board[cell_id] for cell_id in subset)).union({z_a, z_b})
+                values = set("".join(board[cell_id] for cell_id in subset)).union(z)
                 if len(values) == n:
                     impacted_cells = cells
                     for cell in subset:
@@ -206,6 +217,7 @@ def test_3(solver_status, board, window):
                         for candidate in values.intersection(board[cell]):
                             to_remove.add((candidate, cell))
                 if to_remove:
+                    # print(f'\tNaked subset {n = }')
                     return subset
         return None
 
@@ -214,45 +226,35 @@ def test_3(solver_status, board, window):
         cells_by_y = CELLS_IN_COL if by_row else CELLS_IN_ROW
 
         for floor_id_x in range(9):
-            # bi_values.clear()
-            # for floor_id_y in range(9):
-            #     cell = cells_by_x[floor_id_x][floor_id_y]
-            #     if len(board[cell]) == 2:
-            #        bi_values[board[cell]].add((floor_id_x, floor_id_y, CELL_SQR[cell]))
             bi_values = _get_bi_values_dictionary(board, cells_by_x[floor_id_x], by_row)
             for bi_value, coordinates in bi_values.items():
                 if len(coordinates) == 2:
                     floor_a = coordinates.pop()
                     floor_b = coordinates.pop()
-                    xa_ids = _find_xyz(bi_value[0], bi_value[1], cells_by_y[floor_a[1]], by_row)
-                    xb_ids = _find_xyz(bi_value[0], bi_value[1], cells_by_y[floor_b[1]], by_row)
-                    x_ids = xa_ids.intersection(xb_ids)
-                    for x_id in x_ids:
-                        ceiling_a = x_id * 9 + floor_a[1] if by_row else floor_a[1] * 9 + x_id
-                        ceiling_b = x_id * 9 + floor_b[1] if by_row else floor_b[1] * 9 + x_id
-                        boxes = {floor_a[2], floor_b[2], CELL_SQR[ceiling_a], CELL_SQR[ceiling_b]}
-                        if len(boxes) == 2 and board[ceiling_a] != board[ceiling_b]:
-                            z_a = board[ceiling_a].replace(bi_value[0], '').replace(bi_value[1], '')
-                            z_b = board[ceiling_b].replace(bi_value[0], '').replace(bi_value[1], '')
-                            if not to_remove:
-                                naked_subset = _naked_subset(2, ceiling_a, ceiling_b, bi_value, z_a, z_b, by_row)
-                            if not to_remove:
-                                naked_subset = _naked_subset(3, ceiling_a, ceiling_b, bi_value, z_a, z_b, by_row)
-                            if not to_remove:
-                                naked_subset = _naked_subset(4, ceiling_a, ceiling_b, bi_value, z_a, z_b, by_row)
-                            if to_remove:
-                                _set_c_chain(floor_a, floor_b, ceiling_a, ceiling_b, bi_value, (z_a, z_b),
-                                             naked_subset, by_row)
-                                solver_status.capture_baseline(board, window)
-                                remove_options(solver_status, board, to_remove, window)
-                                if window:
-                                    window.options_visible = window.options_visible.union(c_chain.keys())
-                                kwargs["solver_tool"] = "unique_rectangles"
-                                kwargs["c_chain"] = c_chain
-                                kwargs["impacted_cells"] = {cell for _, cell in to_remove}
-                                kwargs["remove"] = to_remove
-                                print('\tTest 3')
-                                return True
+                    for n in (2, 3, 4):
+                        x_ids = _find_xyz(n, bi_value[0], bi_value[1], cells_by_y[floor_a[1]], cells_by_y[floor_b[1]])
+                        for x_id in x_ids:
+                            ceiling_a = x_id * 9 + floor_a[1] if by_row else floor_a[1] * 9 + x_id
+                            ceiling_b = x_id * 9 + floor_b[1] if by_row else floor_b[1] * 9 + x_id
+                            boxes = {floor_a[2], floor_b[2], CELL_SQR[ceiling_a], CELL_SQR[ceiling_b]}
+                            if len(boxes) == 2 and board[ceiling_a] != board[ceiling_b]:
+                                z_a = board[ceiling_a].replace(bi_value[0], '').replace(bi_value[1], '')
+                                z_b = board[ceiling_b].replace(bi_value[0], '').replace(bi_value[1], '')
+                                z_ab = set(z_a).union(z_b)
+                                naked_subset = _naked_subset(n, ceiling_a, ceiling_b, bi_value, z_ab, by_row)
+                                if to_remove:
+                                    _set_c_chain(floor_a, floor_b, ceiling_a, ceiling_b, bi_value, z_ab,
+                                                 naked_subset, by_row)
+                                    solver_status.capture_baseline(board, window)
+                                    remove_options(solver_status, board, to_remove, window)
+                                    if window:
+                                        window.options_visible = window.options_visible.union(c_chain.keys())
+                                    kwargs["solver_tool"] = "unique_rectangles"
+                                    kwargs["c_chain"] = c_chain
+                                    kwargs["impacted_cells"] = {cell for _, cell in to_remove}
+                                    kwargs["remove"] = to_remove
+                                    # print('\tTest 3')
+                                    return True
         return False
 
     init_options(board, solver_status)
